aad84380ca8dff9d54abbabe36f25207c3bb1968
Mihir Sahu <0xmihirsahu@gmail.com>
2024-06-19 16:38:15 +0530

    new slider added with design fix

--- frontend/src/components/InteractionCard.tsx
+++ frontend/src/components/InteractionCard.tsx
@@ -16,8 +16,8 @@ export default function InteractionCard() {
               onClick={() => setTabIndex(1)}
               className={
                 tabIndex === 1
-                  ? "active flex justify-center rounded-tl-md items-center relative mr-7 m-0 pr-7 border bg-primary border-primary border-solid cursor-pointer"
-                  : "flex justify-center rounded-tl-md items-center relative mr-7 m-0 pr-7 border  border-primary border-solid cursor-pointer hover:bg-pink-700"
+                  ? "active flex select-none justify-center rounded-tl-md items-center relative mr-7 m-0 pr-7 border bg-primary border-primary border-solid cursor-pointer"
+                  : "flex justify-center select-none rounded-tl-md items-center relative mr-7 m-0 pr-7 border  border-primary border-solid cursor-pointer hover:bg-pink-700"
               }
               style={{ fontSize: "1.35rem" }}
             >
@@ -29,8 +29,8 @@ export default function InteractionCard() {
               onClick={() => setTabIndex(2)}
               className={
                 tabIndex === 2
-                  ? "active flex justify-center rounded-tr-md items-center relative ml-7 m-0 pl-7 border border-primary bg-primary border-solid cursor-pointer"
-                  : "flex justify-center items-center rounded-tr-md relative ml-7 m-0 pl-7 border border-primary border-solid cursor-pointer hover:bg-pink-700"
+                  ? "active flex select-none justify-center rounded-tr-md items-center relative ml-7 m-0 pl-7 border border-primary bg-primary border-solid cursor-pointer"
+                  : "flex justify-center select-none items-center rounded-tr-md relative ml-7 m-0 pl-7 border border-primary border-solid cursor-pointer hover:bg-pink-700"
               }
               style={{ fontSize: "1.35rem" }}
             >
@@ -44,7 +44,7 @@ export default function InteractionCard() {
         style={{ padding: "1.5rem 1.5rem 2rem" }}
       >
         <div className={tabIndex === 1 ? "content active-content" : "content"}>
-          <TokenSelect />
+          <TokenSelect/>
         </div>
         <div className={tabIndex === 2 ? "content active-content" : "content"}>
           tab2
--- frontend/src/components/TokenSelect.tsx
+++ frontend/src/components/TokenSelect.tsx
@@ -1,9 +1,9 @@
-"use client"
-import React, { useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "./ui/button"
+"use client";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "./ui/button";
 import {
   Form,
   FormControl,
@@ -12,64 +12,68 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "./ui/form"
+} from "./ui/form";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "./ui/select"
-import { toast } from "./ui/use-toast"
-// import { Slider } from "./ui/slider"
-import { Slider } from 'antd';
-import type { SliderSingleProps } from 'antd';
+} from "./ui/select";
+import { toast } from "./ui/use-toast";
+import { Slider } from "antd";
+import type { SliderSingleProps } from "antd";
 
 const FormSchema = z.object({
-  token: z.string({
-    required_error: "Select a token to connect",
-  }),
-})
+  token: z.string().nonempty("Select a token to connect"),
+});
 
-const marks: SliderSingleProps['marks'] = {
+const marks: SliderSingleProps["marks"] = {
   0: {
     style: {
-      color: '#ffffff',
+      color: "#ffffff",
     },
-    label: <p className="text-sm">0.05 </p>,
+    label: <p className="text-sm">0.05</p>,
   },
   25: {
     style: {
-      color: '#ffffff',
-    },
-    label: <p className="text-sm">0.1</p>,
-  },
-  50: {
-    style: {
-      color: '#ffffff',
+      color: "#ffffff",
     },
     label: <p className="text-sm">0.5</p>,
   },
-  75: {
+  50: {
     style: {
-      color: '#ffffff',
+      color: "#ffffff",
     },
     label: <p className="text-sm">1</p>,
   },
+  75: {
+    style: {
+      color: "#ffffff",
+    },
+    label: <p className="text-sm">5</p>,
+  },
   100: {
     style: {
-      color: '#ffffff',
+      color: "#ffffff",
     },
-    label: <p className="text-sm">1.5</p>,
+    label: <p className="text-sm">10</p>,
   },
 };
+
+const valueMap = {
+  0: 0.05,
+  25: 0.5,
+  50: 1,
+  75: 5,
+  100: 10,
+} as const;
+
 export default function TokenSelect() {
-  // const [currentSliderIndex, setCurrentSliderIndex] = useState(0)
-  const [currentValue, setCurrentValue] = useState(0)
-  // const ethereumValues: Array<number> = [0.05, 0.1, 0.5, 1, 2.5]
+  const [currentValue, setCurrentValue] = useState<0.05 | 0.5 | 1 | 5 | 10>(valueMap[0]);
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
-  })
+  });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     toast({
@@ -79,14 +83,14 @@ export default function TokenSelect() {
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       ),
-    })
+    });
   }
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full space-y-6 relative"
+        className="w-full space-y-6 select-none relative"
       >
         <FormField
           control={form.control}
@@ -94,13 +98,13 @@ export default function TokenSelect() {
           render={({ field }) => (
             <FormItem>
               <FormLabel>Token</FormLabel>
-              <Select  onValueChange={field.onChange} defaultValue="Eth">
+              <Select onValueChange={field.onChange} defaultValue="Eth">
                 <FormControl>
-                  <SelectTrigger className="border-primary focus:outline-none focus:shadow-none active:outline-none shadow-none outline-none">
+                  <SelectTrigger className="border-primary focus:outline-none select-none focus:shadow-none active:outline-none shadow-none outline-none">
                     <SelectValue defaultValue="ETH" placeholder="" />
                   </SelectTrigger>
                 </FormControl>
-                <SelectContent className="overflow-hidden absolute z-100 bg-black border-primary">
+                <SelectContent className="overflow-hidden absolute select-none z-100 bg-black border-primary">
                   <SelectItem value="Eth">ETH</SelectItem>
                   <SelectItem value="Starknet">STARK</SelectItem>
                   <SelectItem value="DAI">DAI</SelectItem>
@@ -110,22 +114,19 @@ export default function TokenSelect() {
             </FormItem>
           )}
         />
-        {/* <Slider
-          max={4}
-          min={0}
-          step={1}
-          onValueChange={(value: number[]) => setCurrentSliderIndex(value[0])}
-          className="bg-transparent"
-        /> */}
-        <Slider marks={marks} step={null} defaultValue={20} onChange={(value)=>setCurrentValue(value)} 
-          />
-        <p>current value: {currentValue}</p>
-        {/* @ts-ignore */}
-        {/* // fix needed color is not being applied */}
+        <Slider
+          marks={marks}
+          step={null}
+          defaultValue={0}
+          onChange={(value) =>
+            setCurrentValue(valueMap[value as keyof typeof valueMap])
+          }
+        />
+        <p className="mt-2">current value: {currentValue}</p>
         <Button type="submit" className="bg-primary w-full">
           Connect
         </Button>
       </form>
     </Form>
-  )
+  );
 }
