d656e07a2431ab0a418d3b35a5998f0fb6ac0c11
Paul <mierlo.paul@hotmail.nl>
2024-06-21 17:21:15 +0200

    added color indication withdraw deposit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,216 +1,214 @@
-"use client";
-import AddressBar, { UserModal } from "./AddressBar";
-import { useEffect, useRef, useState } from "react";
-import { useConnect, useAccount } from "@starknet-react/core";
-import TransactionModal from "./TransactionList/TransactionModal";
-import useTheme from "../hooks/useTheme";
-import ThemeSwitch from "./Theme";
-import NetworkSwitcher from "./NetworkSwitcher";
-import ConnectModal from "./ConnectModal";
-import Link from "next/link";
+"use client"
+import AddressBar, { UserModal } from "./AddressBar"
+import { useEffect, useRef, useState } from "react"
+import { useConnect, useAccount } from "@starknet-react/core"
+import TransactionModal from "./TransactionList/TransactionModal"
+import useTheme from "../hooks/useTheme"
+import ThemeSwitch from "./Theme"
+import NetworkSwitcher from "./NetworkSwitcher"
+import ConnectModal from "./ConnectModal"
+import Link from "next/link"
 
 const Header = () => {
-  const { address } = useAccount();
-  const { connect, connectors } = useConnect();
-  const [openConnectModal, setOpenConnectModal] = useState(false);
-  const [openConnectedModal, setOpenConnectedModal] = useState(false);
-  const [isTransactionModalOpen, setIsModalTransactionOpen] = useState(false);
-  const [openMenu, setOpenMenu] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const { address } = useAccount()
+  const { connect, connectors } = useConnect()
+  const [openConnectModal, setOpenConnectModal] = useState(false)
+  const [openConnectedModal, setOpenConnectedModal] = useState(false)
+  const [isTransactionModalOpen, setIsModalTransactionOpen] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false)
+  const dropdownRef = useRef<HTMLDivElement | null>(null)
   const toggleModal = () => {
-    setOpenConnectModal((prev) => !prev);
-  };
+    setOpenConnectModal((prev) => !prev)
+  }
 
   const toggleMenu = () => {
-    setOpenMenu((prev) => !prev);
-  };
+    setOpenMenu((prev) => !prev)
+  }
 
   const toggleUserModal = () => {
-    setOpenConnectedModal((prev) => !prev);
-  };
+    setOpenConnectedModal((prev) => !prev)
+  }
 
   const handleOpenTransactionListClick = () => {
-    setIsModalTransactionOpen(true);
-  };
+    setIsModalTransactionOpen(true)
+  }
 
   const handleCloseTransactionListClick = () => {
-    setIsModalTransactionOpen(false);
-  };
+    setIsModalTransactionOpen(false)
+  }
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
         dropdownRef.current &&
         !dropdownRef.current.contains(event.target as Node)
       ) {
-        setOpenMenu(false);
+        setOpenMenu(false)
       }
-    };
+    }
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside)
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [dropdownRef]);
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [dropdownRef])
 
   useEffect(() => {
     const closeOnEscapeKey = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
-        toggleModal();
+        toggleModal()
       }
-    };
-    document.body.addEventListener("keydown", closeOnEscapeKey);
+    }
+    document.body.addEventListener("keydown", closeOnEscapeKey)
     return () => {
-      document.body.removeEventListener("keydown", closeOnEscapeKey);
-    };
-  }, []);
+      document.body.removeEventListener("keydown", closeOnEscapeKey)
+    }
+  }, [])
 
   useEffect(() => {
-    const lastUsedConnector = localStorage.getItem("lastUsedConnector");
+    const lastUsedConnector = localStorage.getItem("lastUsedConnector")
     if (lastUsedConnector) {
       connect({
         connector: connectors.find(
-          (connector) => connector.name === lastUsedConnector,
+          (connector) => connector.name === lastUsedConnector
         ),
-      });
+      })
     }
-  }, [connectors]);// eslint-disable-line react-hooks/exhaustive-deps
+  }, [connectors]) // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     if (openConnectModal) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = "hidden"
     } else {
-      document.body.style.overflow = "auto";
+      document.body.style.overflow = "auto"
     }
     return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, [openConnectModal]);
+      document.body.style.overflow = "auto"
+    }
+  }, [openConnectModal])
 
-  const { theme, changeTheme } = useTheme();
+  const { theme, changeTheme } = useTheme()
 
   return (
     <>
       <header
         ref={dropdownRef}
-        className="w-full fixed backdrop-blur-2xl dark:border-neutral-800 lg:bg-gray-200 lg:dark:bg-zinc-800/50 left-0 top-0 flex flex-wrap gap-4 py-2 px-4 md:py-4 md:px-10 z-30 justify-between items-center"
+        className=" w-full fixed backdrop-blur-2xl dark:border-neutral-800 lg:bg-gray-200 lg:dark:bg-zinc-800/50 left-0 top-0 flex flex-wrap gap-4 py-2 px-4 md:py-4 md:px-10 z-30 justify-center items-center text-center"
       >
-
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 200 40"
-            width="200"
-            height="40"
-            className="md:text-[1.2em]"
-          ><Link href="/">
-            <text
-              x="10"
-              y="30"
-              fontFamily="Cursive, sans-serif"
-              fill={`${theme === "dark" ? "white" : "black"}`}
+        <div className="flex flex-row " style={{ maxWidth: "1000px" }}>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 200 40"
+              width="200"
+              height="40"
+              className="md:text-[1.2em]"
             >
-              StarkSwirl
-            </text>
-            </Link>
-          </svg>
-        </span>
-
-        <div className="hidden md:flex gap-8">
-          <div className="flex items-center gap-4 text-lg font-medium rounded-lg px-4 py-2 hover:text-primary hover:bg-zinc-900">
-            <Link href="/interaction">
-                App
-            </Link>
-            </div>
-          {address ? (
-            <div className="flex justify-end">
-              <AddressBar setOpenConnectedModal={setOpenConnectedModal} />
-              
-            </div>
-          ) : (
-            <button
-              onClick={toggleModal}
-              className="hidden md:block bg-primary hover:bg-primary text-white  py-2 px-4 rounded-full transition duration-300"
-            >
-              Connect
-            </button>
-          )}
-
-          <NetworkSwitcher />
-
-          <ThemeSwitch
-            className="flex md:hidden lg:hidden sm:hidden dark:transform-none transform dark:translate-none transition-all duration-500 ease-in-out"
-            action={changeTheme}
-            theme={theme}
-            openMenu={openMenu}
-          />
-        </div>
-
-        <div className="flex items-center md:hidden gap-8">
-          <ThemeSwitch
-            className="flex md:hidden dark:transform-none transform dark:translate-none transition-all duration-500 ease-in-out"
-            action={changeTheme}
-            theme={theme}
-            openMenu={openMenu}
-          />
-
-          <button
-            title="toggle menu"
-            onClick={toggleMenu}
-            className="flex flex-col gap-2 md:hidden"
-          >
-            <div
-              className={`w-[1.5em] h-[2px] ${
-                theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
-              } rounded-full transition-all duration-300 ease-in-out ${
-                openMenu
-                  ? "rotate-45 translate-y-[0.625em]"
-                  : "rotate-0 translate-y-0"
-              }`}
-            ></div>
-            <div
-              className={`w-[1.5em] h-[2px] ${
-                theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
-              } rounded-full transition-all duration-300 ease-in-out ${
-                openMenu ? "opacity-0" : "opacity-100"
-              }`}
-            ></div>
-            <div
-              className={`w-[1.5em] h-[2px] ${
-                theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
-              } rounded-full transition-all duration-300 ease-in-out ${
-                openMenu
-                  ? "-rotate-45 translate-y-[-0.625em]"
-                  : "rotate-0 translate-y-0"
-              }`}
-            ></div>
-          </button>
-        </div>
-
-        <div
-          className={`w-screen  transition-all duration-300 ease-in-out grid ${
-            openMenu
-              ? "min-h-[4rem] grid-rows-[1fr]  opacity-100"
-              : "grid-rows-[0fr]  opacity-0"
-          }  md:hidden`}
-        >
-          <div className="overflow-hidden">
-            <div className="flex flex-wrap gap-8">
-              {address ? (
-                <div className="flex justify-end">
-                  <AddressBar setOpenConnectedModal={setOpenConnectedModal} />
-                 
-                </div>
-              ) : (
-                <button
-                  onClick={toggleModal}
-                  className="bg-primary hover:bg-primary text-white py-2 px-4 rounded-full transition duration-300"
+              <Link href="/">
+                <text
+                  x="10"
+                  y="30"
+                  fontFamily="Cursive, sans-serif"
+                  fill={`${theme === "dark" ? "white" : "black"}`}
                 >
-                  Connect
-                </button>
-              )}
+                  StarkSwirl
+                </text>
+              </Link>
+            </svg>
+          </span>
 
-              <NetworkSwitcher />
+          <div className="hidden md:flex gap-8">
+            <div className="flex items-center gap-4 text-lg font-medium rounded-lg px-4 py-2 hover:text-primary hover:bg-zinc-900">
+              <Link href="/interaction">App</Link>
+            </div>
+            {address ? (
+              <div className="flex justify-end">
+                <AddressBar setOpenConnectedModal={setOpenConnectedModal} />
+              </div>
+            ) : (
+              <button
+                onClick={toggleModal}
+                className="hidden md:block bg-primary hover:bg-primary text-white  py-2 px-4 rounded-full transition duration-300"
+              >
+                Connect
+              </button>
+            )}
+
+            <NetworkSwitcher />
+
+            <ThemeSwitch
+              className="flex md:hidden lg:hidden sm:hidden dark:transform-none transform dark:translate-none transition-all duration-500 ease-in-out"
+              action={changeTheme}
+              theme={theme}
+              openMenu={openMenu}
+            />
+          </div>
+
+          <div className="flex items-center md:hidden gap-8">
+            <ThemeSwitch
+              className="flex md:hidden dark:transform-none transform dark:translate-none transition-all duration-500 ease-in-out"
+              action={changeTheme}
+              theme={theme}
+              openMenu={openMenu}
+            />
+
+            <button
+              title="toggle menu"
+              onClick={toggleMenu}
+              className="flex flex-col gap-2 md:hidden"
+            >
+              <div
+                className={`w-[1.5em] h-[2px] ${
+                  theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
+                } rounded-full transition-all duration-300 ease-in-out ${
+                  openMenu
+                    ? "rotate-45 translate-y-[0.625em]"
+                    : "rotate-0 translate-y-0"
+                }`}
+              ></div>
+              <div
+                className={`w-[1.5em] h-[2px] ${
+                  theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
+                } rounded-full transition-all duration-300 ease-in-out ${
+                  openMenu ? "opacity-0" : "opacity-100"
+                }`}
+              ></div>
+              <div
+                className={`w-[1.5em] h-[2px] ${
+                  theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
+                } rounded-full transition-all duration-300 ease-in-out ${
+                  openMenu
+                    ? "-rotate-45 translate-y-[-0.625em]"
+                    : "rotate-0 translate-y-0"
+                }`}
+              ></div>
+            </button>
+          </div>
+
+          <div
+            className={`w-screen  transition-all duration-300 ease-in-out grid ${
+              openMenu
+                ? "min-h-[4rem] grid-rows-[1fr]  opacity-100"
+                : "grid-rows-[0fr]  opacity-0"
+            }  md:hidden`}
+          >
+            <div className="overflow-hidden">
+              <div className="flex flex-wrap gap-8">
+                {address ? (
+                  <div className="flex justify-end">
+                    <AddressBar setOpenConnectedModal={setOpenConnectedModal} />
+                  </div>
+                ) : (
+                  <button
+                    onClick={toggleModal}
+                    className="bg-primary hover:bg-primary text-white py-2 px-4 rounded-full transition duration-300"
+                  >
+                    Connect
+                  </button>
+                )}
+
+                <NetworkSwitcher />
+              </div>
             </div>
           </div>
         </div>
@@ -227,7 +225,7 @@ const Header = () => {
         address={address ? address : ""}
       />
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
